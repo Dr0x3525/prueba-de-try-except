ed9e9566d601f55ba4b946fe577f597a68ec860e
Dr0x3525 <151526410+Dr0x3525@users.noreply.github.com>
2025-06-14 12:10:47 -0500

    este es el primer ejercicio donde voy a usar try-except

---
+++ prueba try-except/1.division_segura/ejercicio_division_segura.py
@@ -0,0 +1,6 @@
+"""
+Escribe un programa que pida dos números al usuario y muestre el resultado de dividir el primero por el segundo.
+Usa try-except para manejar:
+- División por cero
+- Entrada que no sea numérica
+"""
