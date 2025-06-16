87883b6bedc3360d7b047ad8b79e27e55fe0428f
Dr0x3525 <151526410+Dr0x3525@users.noreply.github.com>
2025-06-16 15:14:03 -0500

    voy a comenzar a realizar el ejercicio 2 de prueba

---
+++ prueba try-except/2. Conversión de edad/ejercicio.py
@@ -0,0 +1,5 @@
+"""
+Pide al usuario su edad y conviértela a entero.
+Maneja posibles errores si el usuario no ingresa un número válido.
+Si la edad es negativa, lanza manualmente una excepción con 'raise ValueError'.
+"""
---
+++ prueba try-except/3. Acceso a lista/ejercicio.py
@@ -0,0 +1,7 @@
+"""
+Crea una lista con 5 elementos. Pide al usuario un índice e imprime el elemento en esa posición.
+Maneja:
+- Índice fuera de rango
+- Entrada no numérica
+"""
+
