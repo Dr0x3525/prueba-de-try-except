12d8e3ae282f674290c867ff3f1938de4a6352b3
Dr0x3525 <151526410+Dr0x3525@users.noreply.github.com>
2025-06-16 15:03:22 -0500

    acabe el primer ejercicio

--- prueba try-except/1.division_segura/ejercicio_division_segura.py
+++ prueba try-except/1.division_segura/ejercicio_division_segura.py
@@ -4,3 +4,41 @@ Usa try-except para manejar:
 - División por cero
 - Entrada que no sea numérica
 """
+import os
+
+def limpiar_pantalla():
+    os.system("cls")
+    
+def pausar_pantalla():
+    os.system('pause')
+    
+def pedir_numero():
+    while True:
+        try:
+            numero = int(input(" digita un  numero: "))
+            return numero
+        except ValueError:
+            limpiar_pantalla()
+            print(f'digita un numero entero valido por favor:')
+            pausar_pantalla()
+            limpiar_pantalla()
+
+def division(a,b):
+    limpiar_pantalla()
+    try:
+        respuesta = a / b
+        print(f"{a} / {b} = {respuesta}")
+        return respuesta
+    except ZeroDivisionError:
+      print('no se puede dividir por 0')
+
+
+def inicializar_ejercicio():
+    limpiar_pantalla()
+    numero_1 = pedir_numero()
+    numero_2 = pedir_numero()
+    division(numero_1,numero_2)
+    
+    
+inicializar_ejercicio()
+
