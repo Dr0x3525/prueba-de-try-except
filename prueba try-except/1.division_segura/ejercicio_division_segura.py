a4af5dbd5091623cd223a7ca12f2a49aa0144fb0
drox <drox3525@gmail.com>
2025-06-17 10:25:02 -0500

    acabando el segundo ejercicio

--- prueba try-except/1.division_segura/ejercicio_division_segura.py
+++ prueba try-except/1.division_segura/ejercicio_division_segura.py
@@ -7,38 +7,41 @@ Usa try-except para manejar:
 import os
 
 def limpiar_pantalla():
-    os.system("cls")
+    os.system('clear')
     
 def pausar_pantalla():
-    os.system('pause')
+    input('presiona enter para continuar...')
     
-def pedir_numero():
+def pedir_numero(dato):
     while True:
         try:
-            numero = int(input(" digita un  numero: "))
+            numero = float(input(f'por favor digita el {dato}: '))
             return numero
         except ValueError:
             limpiar_pantalla()
-            print(f'digita un numero entero valido por favor:')
+            print(f'Error: ingresa un numero valido por favor:')
             pausar_pantalla()
             limpiar_pantalla()
 
-def division(a,b):
+def dividir(a,b):
     limpiar_pantalla()
     try:
         respuesta = a / b
         print(f"{a} / {b} = {respuesta}")
-        return respuesta
     except ZeroDivisionError:
-      print('no se puede dividir por 0')
+      print('Error: No se puede dividir entre cero.')
 
 
 def inicializar_ejercicio():
     limpiar_pantalla()
-    numero_1 = pedir_numero()
-    numero_2 = pedir_numero()
-    division(numero_1,numero_2)
+    numero_1 = pedir_numero(dato='primer numero')
+    numero_2 = pedir_numero(dato='segundo numero')
+    dividir(numero_1,numero_2)
     
-    
-inicializar_ejercicio()
 
+inicializar_ejercicio()
+pausar_pantalla()
+limpiar_pantalla()
+print('Fin del ejercicio.')
+pausar_pantalla()
+limpiar_pantalla()
--- prueba try-except/2. Conversión de edad/ejercicio.py
+++ prueba try-except/2. Conversión de edad/ejercicio.py
@@ -3,3 +3,35 @@ Pide al usuario su edad y conviértela a entero.
 Maneja posibles errores si el usuario no ingresa un número válido.
 Si la edad es negativa, lanza manualmente una excepción con 'raise ValueError'.
 """
+
+import os
+
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear') 
+
+def pausar_pantalla():
+    input('Presiona enter para continuar...')
+
+def pedir_edad():
+    while True:
+        try:
+            edad = int(input("cual es tu edad:"))
+            if edad < 0:
+                raise ValueError
+            return edad
+        except ValueError:
+            limpiar_pantalla()
+            print("digita un numero correcto")
+            pausar_pantalla()
+            limpiar_pantalla()
+            
+      
+def inicializa_ejercicio():
+    limpiar_pantalla()
+    pedir_edad()
+    limpiar_pantalla()
+    print("fin del ejercicio")
+    pausar_pantalla()
+    limpiar_pantalla()
+    
+inicializa_ejercicio()
